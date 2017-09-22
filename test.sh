85f66768e68fe6a844623755eeadde5f7e98759a
wangkai37 <wangkai37@baidu.com>
2017-09-22 17:18:28 +0800

    shell传递参数

--- test.sh
+++ test.sh
@@ -39,5 +39,28 @@
 # array_name=(value0 value1 value2 value3)
 # echo ${array_name[n]}
 
-var=http://www.aaa.com/123.htm
-echo ${var#*//}
+# var=http://www.aaa.com/123.htm
+# echo ${var#*//}
+
+# echo "Shell 传递参数实例！"
+# echo "执行的文件名：$0"
+# echo "第一个参数为：$1"
+# echo "第二个参数为：$2"
+# echo "第三个参数为：$3"
+# echo "参数个数为：$#"
+# echo "所有参数：$*"
+# echo "进程ID号：$$"
+# echo "最后一个进程号：$!"
+# echo "所有参数：$@"
+# echo "Shell使用的当前选项：$-"
+# echo "显示最后命令的退出状态：$?"
+
+echo "-- \$* 演示 ---"
+for i in "$*"; do
+    echo $i
+done
+
+echo "-- \$@ 演示 ---"
+for i in "$@"; do
+    echo $i
+done
