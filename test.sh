3a12c1b71a04de925ac020c91e81183904aa762a
wangkai37 <wangkai37@baidu.com>
2017-09-22 17:01:03 +0800

    shell变量

--- test.sh
+++ test.sh
@@ -17,3 +17,24 @@
 # your_name="alibaba"
 # echo $your_name
 
+# myUrl="http://www.w3cschool.cc"
+# readonly myUrl
+# myUrl="http://www.runoob.com"
+
+# myUrl="http://www.runoob.com"
+# unset myUrl
+# echo $myUrl\
+
+# your_name='qinjx'
+# # str="Hello,I know your are \"$your_name\""
+# # echo $str
+# greeting="hello, "$your_name" !"
+# greeting_1="hello,${your_name} !"
+# echo $greeting $greeting_1
+
+# string="runoob is a great site"
+# echo ${string:1:4}
+# echo `expr index "$string" is`
+
+array_name=(value0 value1 value2 value3)
+echo ${array_name[n]}
