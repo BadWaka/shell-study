f014bfb7a1aed9cdcfd947cdfdf19ffd0f291932
wangkai37 <wangkai37@baidu.com>
2017-09-22 19:04:04 +0800

    shell test命令

--- test.sh
+++ test.sh
@@ -171,5 +171,38 @@
 
 # printf "Hello Shell\n"
 
-printf "%-10s %-8s %-4s\n" 姓名 性别 体重kg
-printf "%-10s %-8s %-4.2f\n" 郭靖 男 66.1234
+# printf "%-10s %-8s %-4s\n" 姓名 性别 体重kg
+# printf "%-10s %-8s %-4.2f\n" 郭靖 男 66.1234
+
+# num1=100
+# num2=100
+# if test $[num1] -eq $[num2]
+# then
+#     echo '两个数相等！'
+# else
+#     echo '两个数不相等！'
+# fi
+
+# a=5
+# b=6
+
+# result=$[a+b] # 注意等号两边不能有空格
+# echo "result 为： $result"
+
+# num1="ru1noob"
+# num2="runoob"
+# if test $num1 = $num2
+# then
+#     echo '两个字符串相等!'
+# else
+#     echo '两个字符串不相等!'
+# fi
+
+cd /bin
+if test -e ./notFile -o -e ./bash
+then
+    echo '有一个文件存在!'
+else
+    echo '两个文件都不存在'
+fi
+
