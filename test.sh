d1dba56ea8b1abb4782b66c0af8d884e9cdf4091
wangkai37 <wangkai37@baidu.com>
2017-09-22 16:49:24 +0800

    shell variable

---
+++ test.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+# echo "Hello World!"
+# your_name="runoob.com"
+
+# for file in `ls /etc`
+
+# your_name="qinjx"
+# echo $your_name
+# echo ${your_name}
+
+# for skill in Ada Coffe Action Java; do
+#     echo "I am good at ${skill}Script"
+# done
+
+# your_name="tom"
+# echo $your_name
+# your_name="alibaba"
+# echo $your_name
+
