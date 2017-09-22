40277fc77f5060bb7174458ef47784b8fa0d1ed5
wangkai37 <wangkai37@baidu.com>
2017-09-22 19:10:36 +0800

    流程控制

--- test.sh
+++ test.sh
@@ -198,11 +198,69 @@
 #     echo '两个字符串不相等!'
 # fi
 
-cd /bin
-if test -e ./notFile -o -e ./bash
-then
-    echo '有一个文件存在!'
-else
-    echo '两个文件都不存在'
-fi
+# cd /bin
+# if test -e ./notFile -o -e ./bash
+# then
+#     echo '有一个文件存在!'
+# else
+#     echo '两个文件都不存在'
+# fi
 
+# a=10
+# b=20
+# if [ $a == $b ]
+# then
+#    echo "a 等于 b"
+# elif [ $a -gt $b ]
+# then
+#    echo "a 大于 b"
+# elif [ $a -lt $b ]
+# then
+#    echo "a 小于 b"
+# else
+#    echo "没有符合的条件"
+# fi
+
+# num1=$[2*3]
+# num2=$[1+5]
+# if test $[num1] -eq $[num2]
+# then
+#     echo '两个数字相等!'
+# else
+#     echo '两个数字不相等!'
+# fi
+
+# for loop in 1 2 3 4 5
+# do
+#     echo "The value is: $loop"
+# done
+
+# int=1
+# while(( $int<=5 ))
+# do
+#     echo $int
+#     let "int++"
+# done
+
+# echo '按下 <CTRL-D> 退出'
+# echo -n '输入你最喜欢的网站名: '
+# while read FILM
+# do
+#     echo "是的！$FILM 是一个好网站"
+# done
+
+echo '输入 1 到 4 之间的数字:'
+echo '你输入的数字为:'
+read aNum
+case $aNum in
+    1)  echo '你选择了 1'
+    ;;
+    2)  echo '你选择了 2'
+    ;;
+    3)  echo '你选择了 3'
+    ;;
+    4)  echo '你选择了 4'
+    ;;
+    *)  echo '你没有输入 1 到 4 之间的数字'
+    ;;
+esac
